6c8d080ba6da174f88dabf1b036a4632f138dc2f
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2022-06-18 23:02:55 -0400

    Made the navigation bar, home page, and other menu components.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import Home from "./Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Features from "./components/Features";
+import Pricing from "./components/Pricing";
+import Resources from "./components/Resources";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="features" element={<Features />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="resources" element={<Resources />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
