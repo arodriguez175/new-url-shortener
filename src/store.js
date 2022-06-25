3cc9f23240d7fa2364cec5a590bba5fba4172f2e
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2022-06-24 23:37:35 -0400

    Implemented localStorage to save shortened url's.

--- src/store.js
+++ src/store.js
@@ -1,10 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import shortenerReducer from "./features/urlShortener/shortenerSlice";
+import { loadState, saveState } from "./localStorage";
+import { throttle } from "lodash";
+
+const persistedState = loadState();
 
 const store = configureStore({
   reducer: {
     shortener: shortenerReducer,
   },
+
+  preloadedState: persistedState,
 });
+
+store.subscribe(
+  throttle(() => {
+    saveState(store.getState());
+  }, 1000)
+);
 
 export default store;
